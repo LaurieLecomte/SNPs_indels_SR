93f60c0f76434d300968a3be2c626ce719220fe9
LaurieLecomte <laurieblecomte@gmail.com>
2023-01-26 16:00:35 -0500

    updated CPU requirements

--- 01_scripts/01_call_SNPs_indels.sh
+++ 01_scripts/01_call_SNPs_indels.sh
@@ -2,7 +2,7 @@
 
 # Call SNPs and indels in all samples, parallelized by chromosome
 # Run this script from SNPs_indels_SR directory using this command :
-# parallel -a 02_infos/chr.txt -k -j 10 srun -c 2 --mem=10G -p ibis_medium --time=7-00:00:00 -J 01_call_SNPs_indels_{} -o log/01_call_SNPs_indels_{}_%j.log 01_scripts/01_call_SNPs_indels.sh {} &
+# parallel -a 02_infos/chr.txt -k -j 10 srun -c 4 --mem=10G -p ibis_medium --time=7-00:00:00 -J 01_call_SNPs_indels_{} -o log/01_call_SNPs_indels_{}_%j.log 01_scripts/01_call_SNPs_indels.sh {} &
 
 
 # VARIABLES
@@ -10,7 +10,7 @@ REF="03_genome/genome.fasta"     # reference genome
 BAM_LIST="02_infos/bam_list.txt" # list of bam files
 CHR=$1                           # region or chromosome for this job 
 CALLS_DIR="05_calls"              # results directory
-CPU=2                            # number of threads to use per job
+CPU=4                            # number of threads to use per job
 
 # LOAD REQUIRED MODULES
 module load bcftools/1.16
